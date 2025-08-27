047bce797c363f11e89850d7788d7709a809d3c4
Vijay Raghav <raghavvijayraj@gmail.com>
2025-08-27 15:19:37 +0530

    initial commit

---
+++ ProductTests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿
+using product;
+
+namespace ProductTests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Add_TwoNumbers_ReturnsSum()
+    {
+        var result = Pro.Add(2, 3);
+        Assert.Equal(5, result);
+    }
+}
---
+++ product/Prod.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace product
+{
+    public class Pro
+    {
+        public static int Add(int a, int b) => a + b;
+    }
+}
