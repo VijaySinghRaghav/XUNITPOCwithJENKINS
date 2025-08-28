3b47f34610db6ddcf4fa8919f55790fa18240cac
Vijay Raghav <raghavvijayraj@gmail.com>
2025-08-28 21:45:15 +0530

    initial commit 2

--- ProductTests/obj/Debug/net9.0/ProductTests.AssemblyInfo.cs
+++ ProductTests/obj/Debug/net9.0/ProductTests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProductTests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+047bce797c363f11e89850d7788d7709a809d3c4")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProductTests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProductTests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- product/obj/Debug/net9.0/product.AssemblyInfo.cs
+++ product/obj/Debug/net9.0/product.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("product")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+047bce797c363f11e89850d7788d7709a809d3c4")]
 [assembly: System.Reflection.AssemblyProductAttribute("product")]
 [assembly: System.Reflection.AssemblyTitleAttribute("product")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
